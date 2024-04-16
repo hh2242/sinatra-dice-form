71d0356dfbd4b61a902aead3fba0e87815620835
hh2242 <165195157+hh2242@users.noreply.github.com>
2024-04-16 00:36:15 +0000

    g

--- app.rb
+++ app.rb
@@ -4,3 +4,19 @@ require "sinatra/reloader"
 get("/") do
   erb(:main)
 end
+
+get("/process_form") do
+  @num_dice = params.fetch("dice").to_i
+
+  @sides = params.fetch("sides").to_i
+
+  @rolls = []
+
+  @num_dice.times do
+    die = rand(1..@sides)
+
+    @rolls.push(die)
+  end
+
+  erb(:flexible)
+end
